e453fd7d643291394acc952621a3c426b43a5491
AnastasiiaKochurenkovaPM <anastasiia.kochurenkova.pm.2020@lpnu.ua>
2022-08-30 17:44:21 +0300

    Unit_test_abs

---
+++ Calculator/CalcClassBrTest/CalcTest.cs
@@ -0,0 +1,31 @@
+﻿using CalcClassBr;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource("System.Data.SqlClient", @"Data Source=(localdb)\MSSQLLocalDB; AttachDbFilename=C:\Users\KV-User\Desktop\astpp\Calculator\CalcClassBrTest\bin\Debug\ru\test.mdf; Integrated Security=True; Connect Timeout=30", "Table", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void ABS_FromDB()
+        {
+            long a = (long)TestContext.DataRow["actual"];
+            string res = (string)TestContext.DataRow["result"];
+            long actual;
+            try
+            {
+                actual = CalcClass.ABS(a);
+                Assert.AreEqual(Convert.ToInt64(res), actual);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Assert.AreEqual(res, "Error");
+            }
+        }
+    }
+}
---
+++ Calculator/CalcClassBrTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcClassBrTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcClassBrTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("782fb1bb-bafe-4f1e-a0f5-1fe080b54d67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
